9700bfa51d5c8e6b62ea51f24901de2962a7eca4
KyleHS <k15156@yahoo.com>
2023-07-06 18:43:32 -0600

    project finished.

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-const franc = require("franc");
-const langs = require("langs");
+import {franc} from 'franc'; 
+import langs from 'langs';
+import colors from 'colors';
+
+const userInput = process.argv[2];
+const langCode = franc(userInput);
+
+if (langCode === 'und') {
+    console.log(colors.red("Input too short. Please try with more sample text"));
+} else {
+    const language = langs.where("3", langCode);
+    if (language) {
+        console.log(colors.green(`Your language is: ${language.name}`));
+    } else {
+        console.log(colors.red('Sorry, language code was not found'));
+    }
+    
+};
